1a14ba5e2e0db7e2aceccdcdf857a6b3e99673fd
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-08-09 11:02:58 -0500

    initial setup

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store} >
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
